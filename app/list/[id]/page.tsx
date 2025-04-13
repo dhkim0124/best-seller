a5f037408e69249c15f78cbf52ea218dedfe37f8
dhkim0124 <dhkim92@icloud.com>
2025-04-14 05:18:55 +0900

    Refactor ListPage component to remove unnecessary await for params

--- app/list/[id]/page.tsx
+++ app/list/[id]/page.tsx
@@ -1,4 +1,4 @@
-export default async function ListPage({ params }: { params: { id: string } }) {
-  const { id } = await params;
+export default function ListPage({ params }: { params: { id: string } }) {
+  const { id } = params;
   return <div>ListPage {id}</div>;
 }
