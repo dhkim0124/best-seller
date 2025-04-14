36c009b2e6369b36e328419b9e82197ce24ebda1
dhkim0124 <dhkim92@icloud.com>
2025-04-14 23:01:55 +0900

    style: update navbar

--- components/nave-bar.tsx
+++ components/nave-bar.tsx
@@ -2,9 +2,13 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <div>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
+    <div className="flex justify-between items-center p-4 text-xl font-bold px-20 bg-gray-900 mb-10">
+      <div className="border-2 border-white-500 py-1 px-2 rounded-md hover:bg-white hover:text-black">
+        <Link href="/">Home</Link>
+      </div>
+      <div className="border-2 border-white-500 py-1 px-2 rounded-md hover:bg-white hover:text-black">
+        <Link href="/about">About Us</Link>
+      </div>
     </div>
   );
 }
